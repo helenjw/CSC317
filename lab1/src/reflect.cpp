e8d6808c828b23af0ef92bb25cc3a51dc409a580
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-01-16 22:44:37 -0700

    mirror implemented

--- lab1/src/reflect.cpp
+++ lab1/src/reflect.cpp
@@ -8,7 +8,25 @@ void reflect(
   std::vector<unsigned char> & reflected)
 {
   reflected.resize(width*height*num_channels);
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+  int left = 0;
+  int right = width - 1;
+
+  while (left <= right) {
+    for (int row = 0; row < height; row++) {
+      
+      for (int channel = 0; channel < num_channels; channel++) {
+        int l_val = input[channel + num_channels * (width * row + left)];
+        int r_val = input[channel + num_channels * (width * row + right)];
+
+        reflected[channel + num_channels * (width * row + left)] = r_val;
+        reflected[channel + num_channels * (width * row + right)] = l_val;
+      }
+
+    }
+
+    left++;
+    right--;
+  }
+
+  return;
 }
