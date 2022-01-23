8754ca02e90bb343845b5b382ed0cdcf31a56e9a
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-01-23 16:29:59 -0700

    porter duff composite

--- lab1/src/over.cpp
+++ lab1/src/over.cpp
@@ -8,7 +8,24 @@ void over(
   std::vector<unsigned char> & C)
 {
   C.resize(A.size());
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+  
+  // Overlay A on top of B
+  for (int row = 0; row < height; row++) {
+    for (int col = 0; col < width; col++) {
+      int i = 4 * (col + row * width);
+
+      double alpha_a = A[i + 3] / 255.0;
+      double alpha_b = B[i + 3] / 255.0;
+      double alpha_c = alpha_a + alpha_b * (1 - alpha_a);
+      C[i + 3] = 255.0 * alpha_c;
+
+      for (int color = 0; color < 3; color++) {
+        double c_a = A[i + color];
+        double c_b = B[i + color];
+        C[i + color] = ((alpha_a * c_a) + (c_b * alpha_b * (1 - alpha_a))) / alpha_c;
+      }
+    }
+  }
+  
+  return;
 }
