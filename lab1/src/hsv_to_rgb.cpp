6672a57964a928a8e8f76543df945e2434c487a6
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-01-23 14:21:41 -0700

    hsv_to_rgb wikipedia version

--- lab1/src/hsv_to_rgb.cpp
+++ lab1/src/hsv_to_rgb.cpp
@@ -1,4 +1,5 @@
 #include "hsv_to_rgb.h"
+#include <cmath>
 
 void hsv_to_rgb(
   const double h,
@@ -8,10 +9,49 @@ void hsv_to_rgb(
   double & g,
   double & b)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  r = 0;
-  g = 0;
-  b = 0;
-  ////////////////////////////////////////////////////////////////////////////
+  double c = v * s;
+  double h_prime = h / 60.0;
+  double x = c * (1.0 - abs(fmod(h_prime, 2.0) - 1.0)); // X = C * (1 - \H' % 2 - 1\)
+
+  if (0 <= h_prime && h_prime < 1) {
+    r = c;
+    g = x;
+    b = 0;
+  }
+  else if (1 <= h_prime && h_prime < 2)
+  {
+    r = x;
+    g = c;
+    b = 0;
+  }
+  else if (2 <= h_prime && h_prime < 3)
+  {
+    r = 0;
+    g = c;
+    b = x;
+  }
+  else if (3 <= h_prime && h_prime < 4)
+  {
+    r = 0;
+    g = x;
+    b = c;
+  }
+  else if (4 <= h_prime && h_prime < 5)
+  {
+    r = x;
+    g = 0;
+    b = c;
+  }
+  else if (5 <= h_prime && h_prime < 6)
+  {
+    r = c;
+    g = 0;
+    b = x;
+  }
+
+  double m = v - c;
+  r += m;
+  g += m;
+  b += m;
+  return;
 }
