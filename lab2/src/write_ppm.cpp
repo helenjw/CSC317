f5fbbd301b5fbdd841bcc77151f5797922c284b8
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-01-24 12:06:36 -0700

    Fix build

--- lab2/src/write_ppm.cpp
+++ lab2/src/write_ppm.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cassert>
 #include <iostream>
+using namespace std;
 
 bool write_ppm(
   const std::string & filename,
@@ -10,8 +11,26 @@ bool write_ppm(
   const int height,
   const int num_channels)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code from computer-graphics-raster-images
-  return false;
-  ////////////////////////////////////////////////////////////////////////////
+  assert((num_channels == 3 || num_channels == 1 ) && ".ppm only supports RGB or grayscale images");
+
+  ofstream file(filename);
+  file << ((num_channels == 3) ? "P3\t" : "P2\t") << endl;
+  file << width << "\t" << height << endl;
+  file << "255" << endl;
+
+  for (int row = 0; row < height; row++) {
+    for (int col = 0; col < width; col++) {
+
+      for (int channel = 0; channel < num_channels; channel++) {
+        file << (int)data[channel + num_channels * (col + row * width)] << " ";
+      }
+
+      file << "\t";
+    }
+
+    file << endl;
+  }
+
+  file.close();
+  return true;
 }
