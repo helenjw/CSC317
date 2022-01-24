d19756b71a6f2148807eaba2f867196cafd373ba
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-01-24 16:21:37 -0700

    viewing_ray and first_hit calculations

--- lab2/src/first_hit.cpp
+++ lab2/src/first_hit.cpp
@@ -1,4 +1,5 @@
 #include "first_hit.h"
+using namespace std;
 
 bool first_hit(
   const Ray & ray, 
@@ -8,9 +9,27 @@ bool first_hit(
   double & t,
   Eigen::Vector3d & n)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  return false;
-  ////////////////////////////////////////////////////////////////////////////
+  int smallest_t = numeric_limits<int>::max();
+  hit_id = -1;
+  Eigen::Vector3d normal;
+
+  // Iterate through each object and see if ray intersects with anything. Update if it does
+  for (int i = 0; i < objects.size(); i++) {
+    if (objects[i] -> intersect(ray, min_t, t, n)) {
+      if (smallest_t > t) {
+        smallest_t = t;
+        hit_id = i;
+        normal = n;
+      }
+    }
+  }
+
+  if (hit_id < 0) {
+    return false;
+  }
+
+  t = smallest_t;
+  n = normal;
+  return true;
 }
 
--- lab2/src/viewing_ray.cpp
+++ lab2/src/viewing_ray.cpp
@@ -8,8 +8,14 @@ void viewing_ray(
   const int height,
   Ray & ray)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+  // Orthographic viewing ray - chapter 4.3.1
+  // u = l + (r - l)(j + 0.5)/nx
+  int u = -camera.width / 2 + camera.width * (j + 0.5) / width;
+
+  // v = b + (t - b)(i + 0.5)/ny
+  int v = -camera.height / 2 + camera.height * (i + 0.5) / height;
+
+  ray.direction = -camera.w;
+  ray.origin = camera.u * u + camera.v * v - camera.d * camera.w;
 }
 
