dd9bc325710e319c156a0d77c33cb80aada7a843
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-01-25 17:16:50 -0700

    Fixed plane errors

--- lab2/src/Plane.cpp
+++ lab2/src/Plane.cpp
@@ -1,24 +1,22 @@
 #include "Plane.h"
 #include "Ray.h"
+#include <iostream>
 using namespace Eigen;
+using namespace std;
 
 bool Plane::intersect(
   const Ray & ray, const double min_t, double & t, Eigen::Vector3d & n) const
 {
   Eigen::Vector3d n_plane, d, e, po;
-  n_plane = this->normal.normalized();
-  d = ray.direction.normalized();
+  n_plane = this->normal;
+  d = ray.direction;
   e = ray.origin;
   po = this->point;
 
-  // t = ( -n · (e - po) ) / ( n · d )
-  t = n_plane.dot(d) != 0 ? ( -n_plane.dot(e - po) ) / ( n_plane.dot(d) ) : std::numeric_limits<double>::min();
+  // t = ( -n · (e - po) ) / ( n · d ) - but first check division by 0
+  double denom = n_plane.dot(d);
+  t = denom != 0 ? -n_plane.dot(e - po) / denom : numeric_limits<double>::min();
   n = n_plane.normalized();
 
-  if (t < min_t) {
-    return false;
-  }
-
-  return true;
+  return t >=  min_t ? true : false;
 }
-
