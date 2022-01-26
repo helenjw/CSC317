7df48f6595c2a87766da214bfdf53400c99a6bc3
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-01-25 23:01:38 -0700

    Fixed inside-sphere

--- lab2/src/Sphere.cpp
+++ lab2/src/Sphere.cpp
@@ -2,6 +2,7 @@
 #include "Ray.h"
 #include <cmath>
 using namespace Eigen;
+using namespace std;
 
 bool Sphere::intersect(
   const Ray & ray, const double min_t, double & t, Eigen::Vector3d & n) const
@@ -18,38 +19,24 @@ bool Sphere::intersect(
   C = (e - c).dot(e - c) - r * r;
 
   // Calculate discriminant: B^2 - 4AC
-  int disc = B * B - 4 * A * C;
+  double disc = B * B - 4 * A * C;
 
   // no intersection
   if (disc < 0) {
     return false;
   }
 
-  // Ray grazes sphere
-  else if (disc == 0) {
-    t = - B / (2 * A);
+  double t1 = (-B - sqrt(disc)) / (2 * A);
+  double t2 = (-B + sqrt(disc)) / (2 * A);
+  
+  // if both points behind the camera
+  if (t1 < min_t && t2 < min_t) {
+    return false;
   }
 
-  // Ray enters AND leaves sphere - choose closest one
-  else {
-    double t1 = (-B - sqrt(disc)) / (2 * A);
-    double t2 = (-B + sqrt(disc)) / (2 * A);
-    
-    // if both points behind the camera
-    if (t1 < min_t && t2 < min_t) {
-      return false;
-    }
-
-    // point at t1 behind camera, but t2 is visible
-    else if (t1 < min_t)
-    {
-      t = t2;
-    }
-
-    // both t1 and t2 are visible - take min time since that would be closer
-    else {
-      t = std::min(t1, t2);
-    }
+  t = min(t1, t2);
+  if (t < min_t) {
+    t = max(t1, t2);
   }
 
   // Normalize: n = (p - c).normalized(), p = point of intersection
