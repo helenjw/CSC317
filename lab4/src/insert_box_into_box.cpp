f8a61e7512a17459623025549cbc78c70033f4ec
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-02-06 15:32:24 -0500

    🥳 Implemented insert box into box

--- lab4/src/insert_box_into_box.cpp
+++ lab4/src/insert_box_into_box.cpp
@@ -1,11 +1,18 @@
 #include "insert_box_into_box.h"
+using namespace std;
 
 void insert_box_into_box(
   const BoundingBox & A,
   BoundingBox & B)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+  for (int axis = 0; axis < 3; axis++) {
+    // Min corner: take the smallest
+    B.min_corner(axis) = min(B.min_corner(axis), A.min_corner(axis));
+
+    // Max corner: take the largest
+    B.max_corner(axis) = max(B.max_corner(axis), A.max_corner(axis));
+  }
+
+  return;
 }
 
