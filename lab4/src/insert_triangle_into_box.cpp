8386470b30a8bbf627385eba592190a545344530
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-02-06 15:51:03 -0500

    🦫  Implemented insert triangle into box

--- lab4/src/insert_triangle_into_box.cpp
+++ lab4/src/insert_triangle_into_box.cpp
@@ -1,4 +1,6 @@
 #include "insert_triangle_into_box.h"
+using namespace Eigen;
+using namespace std;
 
 void insert_triangle_into_box(
   const Eigen::RowVector3d & a,
@@ -6,9 +8,13 @@ void insert_triangle_into_box(
   const Eigen::RowVector3d & c,
   BoundingBox & B)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+  for (int axis = 0; axis < 3; axis++) {
+    // Compare a, b, c, and B and take smallest for min
+    B.min_corner(axis) = min({B.min_corner(axis), a[axis], b[axis], c[axis]});
+
+    // Compare a, b, c, and B and take largest for max
+    B.max_corner(axis) = max({B.max_corner(axis), a[axis], b[axis], c[axis]});
+  }
 }
 
 
