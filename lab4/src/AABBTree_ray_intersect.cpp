76f5d76efae2a2b8d13993ef6de62d60b77d8d61
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-02-06 18:21:18 -0500

    🥞  Implemented AABBTree ray intersect

--- lab4/src/AABBTree_ray_intersect.cpp
+++ lab4/src/AABBTree_ray_intersect.cpp
@@ -1,4 +1,5 @@
 #include "AABBTree.h"
+using namespace std;
 
 // See AABBTree.h for API
 bool AABBTree::ray_intersect(
@@ -8,10 +9,33 @@ bool AABBTree::ray_intersect(
   double & t,
   std::shared_ptr<Object> & descendant) const 
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  t = 0;
-  return false;
-  ////////////////////////////////////////////////////////////////////////////
+  // Check if the ray hits the box before considering descendants
+  if ( !ray_intersect_box(ray, this->box, min_t, max_t) ) return false;
+
+  // Check the left and right children and see if we have a hit
+  shared_ptr<Object> leftChildren, rightChildren;
+  double t_right, t_left;
+
+  bool left_hit = (this->left) ? this->left->ray_intersect(ray, min_t, max_t, t_left, leftChildren) : false;
+  bool right_hit = (this->right) ? this->right->ray_intersect(ray, min_t, max_t, t_right, rightChildren) : false;
+
+  // No hit
+  if ( !left_hit && !right_hit ) return false;
+
+  // 1+ hit(s)
+  if (left_hit && right_hit) {
+    t = min(t_left, t_right);
+    descendant = (t_left < t_right) ? leftChildren : rightChildren;
+
+  } else if (left_hit) {
+    t = t_left;
+    descendant = leftChildren;
+
+  } else if (right_hit) {
+    t = t_right;
+    descendant = rightChildren;
+  } 
+  
+  return true;
 }
 
