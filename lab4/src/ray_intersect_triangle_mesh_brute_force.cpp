c4f6e1e83635c3a01a8da8671dae1420e4985393
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-02-06 11:23:09 -0500

    🥐 Implemented ray_intersect_triangle_mesh bf

--- lab4/src/ray_intersect_triangle_mesh_brute_force.cpp
+++ lab4/src/ray_intersect_triangle_mesh_brute_force.cpp
@@ -1,5 +1,6 @@
 #include "ray_intersect_triangle_mesh_brute_force.h"
 #include "ray_intersect_triangle.h"
+using namespace Eigen;
 
 bool ray_intersect_triangle_mesh_brute_force(
   const Ray & ray,
@@ -10,10 +11,32 @@ bool ray_intersect_triangle_mesh_brute_force(
   double & hit_t,
   int & hit_f)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  hit_t = 0;
-  hit_f = 0;
+  double smallest_t;
+  int smallest_f = -1;
+
+  // Iterate through all the triangles
+  for (int i = 0; i < F.rows(); i++) {
+    RowVector3d A = V.row(F(i, 0));
+    RowVector3d B = V.row(F(i, 1));
+    RowVector3d C = V.row(F(i, 2));
+
+    double temp_hit_t;
+    // Check intersection
+    if ( ray_intersect_triangle(ray, A, B, C, min_t, max_t, temp_hit_t) ) {
+
+      // Keep the smallest t and associated hit_id
+      if (smallest_t > temp_hit_t) {
+        smallest_t = temp_hit_t;
+        smallest_f = i;
+      }
+    }
+  }
+
+  if (smallest_f > -1) {
+    hit_t = smallest_t;
+    hit_f = smallest_f;
+    return true;
+  }
+
   return false;
-  ////////////////////////////////////////////////////////////////////////////
 }
