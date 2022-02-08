901fbe89644a5bf3968135e9284b26c7b92c6edb
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-02-08 15:40:56 -0500

    🐙 Implemented point box sqred distance

--- lab4/src/point_box_squared_distance.cpp
+++ lab4/src/point_box_squared_distance.cpp
@@ -1,11 +1,25 @@
 #include "point_box_squared_distance.h"
+using namespace Eigen;
+using namespace std;
 
 double point_box_squared_distance(
   const Eigen::RowVector3d & query,
   const BoundingBox & box)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here
-  return 0;
-  ////////////////////////////////////////////////////////////////////////////
+  double ans = 0;
+
+  for (int axis = 0; axis < 3; axis++) {
+    // If we are already inside the box, the distance is 0
+    if (box.min_corner(axis) <= query[axis] && query[axis] <= box.max_corner(axis)){
+      continue;
+    }
+
+    double dist_to_min = abs(query[axis] - box.min_corner(axis));
+    double dist_to_max = abs(query[axis] - box.max_corner(axis));
+
+    // Add the square of which ever distance is smaller
+    ans += (dist_to_max > dist_to_min) ? dist_to_max * dist_to_max : dist_to_min * dist_to_min;
+  }
+
+  return ans;
 }
