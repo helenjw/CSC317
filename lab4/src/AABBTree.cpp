943fe682cbcb0c282aab7bd73428204b3647808d
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-02-07 23:12:23 -0500

    🥴 Fix too many boxes being drawn

--- lab4/src/AABBTree.cpp
+++ lab4/src/AABBTree.cpp
@@ -11,11 +11,10 @@ AABBTree::AABBTree(
   num_leaves(objects.size())
 {
   // Construct the leaf nodes if we are <= 2 objects
-  int n = objects.size();
   this->left = NULL;
   this->right = NULL;
 
-  switch (n) {
+  switch (num_leaves) {
     case 0:
       return;
 
@@ -37,42 +36,51 @@ AABBTree::AABBTree(
   }
 
   // Insert all the objects into current box
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < num_leaves; i++) {
     insert_box_into_box(objects[i]->box, this->box);
   }
 
   // Find the longest axis
-  int axis;
-  double longest_len = -1;
-
+  vector<double> axis_lengths;
   for (int a = 0; a < 3; a++) {
-    double length = abs(this->box.max_corner(a) - this->box.min_corner(a));
-    if (length > longest_len){
-      longest_len = length;
-      axis = a;
-    }
+    axis_lengths.push_back( abs(this->box.max_corner(a) - this->box.min_corner(a)) );
   }
+  int axis = distance(axis_lengths.begin(),max_element(axis_lengths.begin(), axis_lengths.end()));
 
   // Split objects along the axis
   double mid = this->box.center()[axis];
 
-  // Lambda function to sort the list by items smaller than midpoint and larger
-  vector< shared_ptr<Object> > sortedObjects(objects);
-  auto fct = [&axis](const shared_ptr<Object> &a, const shared_ptr<Object> &b) {
-    return a->box.center()[axis] < b->box.center()[axis];
-  };
-  sort(sortedObjects.begin(), sortedObjects.end(), fct);
-
-  // Put half the sorted list on the left and the other on the right
-  // I do this because if I partition objects on the l & r based on midpoint, one list can be empty and the function can infinitely recurse
   vector< shared_ptr<Object> > leftside;
   vector< shared_ptr<Object> > rightside;
-  for (int i = 0; i < n; i++) {
-    if (i < n / 2) {
+
+  for (int i = 0; i < num_leaves; i++) {
+    if (objects[i]->box.center()[axis] < mid) {
       leftside.push_back(objects[i]);
-    }
-    else {
+    } else {
       rightside.push_back(objects[i]);
+    }
+  }
+
+  // If nothing was done previously, we need to split the list by 2 to avoid infinite recursion
+  if (leftside.size() == 0 || rightside.size() == 0) {
+    leftside.clear();
+    rightside.clear();
+
+    // Lambda function to sort the list by locations of centers
+    vector< shared_ptr<Object> > sortedObjects(objects);
+    auto fct = [&axis](const shared_ptr<Object> &a, const shared_ptr<Object> &b) {
+      return a->box.center()[axis] < b->box.center()[axis];
+    };
+    sort(sortedObjects.begin(), sortedObjects.end(), fct);
+
+    // Put half the sorted list on the left and the other on the right
+    for (int i = 0; i < num_leaves; i++) {
+      if (i < num_leaves / 2) {
+        leftside.push_back(sortedObjects[i]);
+      }
+      else {
+        rightside.push_back(sortedObjects[i]);
+      }
     }
   }
 
