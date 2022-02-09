a5ef02fd8d52929820eb3a35b39efad0c0df23b5
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-02-08 22:49:43 -0500

    🤧 Clean up code and fix triangle int triangle

--- lab4/src/triangle_triangle_intersection.cpp
+++ lab4/src/triangle_triangle_intersection.cpp
@@ -14,8 +14,11 @@ bool triangle_triangle_intersection(
   // Take triangle A, and make each edge a "ray". See if the ray hits triangle B. Vice versa
   if (
     ray_intersect_triangle(Ray(A0, A1 - A0), B0, B1, B2, 0, 1, t) ||
-    ray_intersect_triangle(Ray(A1, A1 - A2), B0, B1, B2, 0, 1, t) ||
-    ray_intersect_triangle(Ray(A2, A0 - A2), B0, B1, B2, 0, 1, t)
+    ray_intersect_triangle(Ray(A1, A2 - A1), B0, B1, B2, 0, 1, t) ||
+    ray_intersect_triangle(Ray(A2, A0 - A2), B0, B1, B2, 0, 1, t) ||
+    ray_intersect_triangle(Ray(B0, B1 - B0), A0, A1, A2, 0, 1, t) ||
+    ray_intersect_triangle(Ray(B1, B2 - B1), A0, A1, A2, 0, 1, t) ||
+    ray_intersect_triangle(Ray(B2, B0 - B2), A0, A1, A2, 0, 1, t)
   ) {
     return true;
   }
