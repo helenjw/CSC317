2bc073e4255c9d6acd6b070abe35ac38299481db
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-02-08 21:29:33 -0500

    🔮 Implement box and triangle intesections

--- lab4/src/box_box_intersect.cpp
+++ lab4/src/box_box_intersect.cpp
@@ -3,9 +3,14 @@ bool box_box_intersect(
   const BoundingBox & A,
   const BoundingBox & B)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  return false;
-  ////////////////////////////////////////////////////////////////////////////
+  for (int axis = 0; axis < 3; axis++) {
+    // if A & B intersect, then A's largest corner cannot be smaller than B's smallest corner
+    if (B.min_corner(axis) > A.max_corner(axis)) return false;
+
+    // also, if A & B intersect, then A's smallest corner cannot be larger than B's largest corner
+    if (B.max_corner(axis) < A.min_corner(axis)) return false;
+  }
+
+  return true;
 }
 
--- lab4/src/triangle_triangle_intersection.cpp
+++ lab4/src/triangle_triangle_intersection.cpp
@@ -1,4 +1,5 @@
 #include "triangle_triangle_intersection.h"
+#include "ray_intersect_triangle.h"
 
 bool triangle_triangle_intersection(
   const Eigen::RowVector3d & A0,
@@ -8,8 +9,16 @@ bool triangle_triangle_intersection(
   const Eigen::RowVector3d & B1,
   const Eigen::RowVector3d & B2)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code here:
-  return false; 
-  ////////////////////////////////////////////////////////////////////////////
+  double t;
+
+  // Take triangle A, and make each edge a "ray". See if the ray hits triangle B
+  if (
+    ray_intersect_triangle(Ray(A0, A1-A0), B0, B1, B2, 0, 1, t) ||
+    ray_intersect_triangle(Ray(A1, A1-A2), B0, B1, B2, 0, 1, t) ||
+    ray_intersect_triangle(Ray(A2, A2-A0), B0, B1, B2, 0, 1, t)
+  ) {
+    return true;
+  }
+
+  return false;
 }
