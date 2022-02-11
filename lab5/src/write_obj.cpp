19885aeccacf208e7fcf8b95d385129839717fb8
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-02-11 10:18:06 -0500

    🍑 Implemented write_obj

--- lab5/src/write_obj.cpp
+++ lab5/src/write_obj.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cassert>
 #include <iostream>
+using namespace std;
 
 bool write_obj(
   const std::string & filename,
@@ -13,8 +14,37 @@ bool write_obj(
   const Eigen::MatrixXi & NF)
 {
   assert((F.size() == 0 || F.cols() == 3 || F.cols() == 4) && "F must have 3 or 4 columns");
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here:
-  ////////////////////////////////////////////////////////////////////////////
-  return false;
+  
+  // Reference: https://en.wikipedia.org/wiki/Wavefront_.obj_file
+  ofstream file(filename);
+
+  // Write vertex
+  for (int i = 0; i < V.rows(); i++) {
+    file << "v " << V(i, 0) << " " << V(i, 1) << " " << V(i, 2) << endl;
+  }
+
+  // Write vertex texture 
+  for (int i = 0; i < UV.rows(); i++) {
+    file << "vt " << UV(i, 0) << " " << UV(i, 1) << " " << UV(i, 2) << endl;
+  }
+
+  // Write vertex normal 
+  for (int i = 0; i < NV.rows(); i++) {
+    file << "vn " << NV(i, 0) << " " << NV(i, 1) << " " << NV(i, 2) << endl;
+  }
+
+  // Write face: Faces are defined using lists of vertex, texture and normal indices 
+  // in the format <vertex_index> / <texture_index> / <normal_index>
+  for (int i = 0; i < F.rows(); i++) {
+    file << "f ";
+
+    for (int j = 0; j < F.cols(); j++) {
+      file << F(i, j) + 1 << "/" << UF(i, j) + 1 << "/" << NF(i, j) << " "; // .obj file is 1 indexed
+    }
+
+    file << endl;
+  }
+
+  file.close();
+  return true;
 }
