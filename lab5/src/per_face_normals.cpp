62e16f2ce986132d6d1fe1ae952713096b1786ae
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-02-15 09:59:51 -0500

    🍑 Implemented per-face normals

--- lab5/src/per_face_normals.cpp
+++ lab5/src/per_face_normals.cpp
@@ -1,13 +1,22 @@
 #include "per_face_normals.h"
 #include "triangle_area_normal.h"
+using namespace Eigen;
 
 void per_face_normals(
   const Eigen::MatrixXd & V,
   const Eigen::MatrixXi & F,
   Eigen::MatrixXd & N)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code:
-  N = Eigen::MatrixXd::Zero(F.rows(),3);
-  ////////////////////////////////////////////////////////////////////////////
+  N.resize(F.rows(), 3);
+
+  for (int triangle = 0; triangle < F.rows(); triangle++) {
+    int a = F(triangle, 0);
+    int b = F(triangle, 1);
+    int c = F(triangle, 2);
+
+    N.row(triangle) = triangle_area_normal(V.row(a), V.row(b), V.row(c));
+  }
+
+  return;
 }
+
--- lab5/src/triangle_area_normal.cpp
+++ lab5/src/triangle_area_normal.cpp
@@ -1,13 +1,13 @@
 #include "triangle_area_normal.h"
 #include <Eigen/Geometry>
+using namespace Eigen;
 
 Eigen::RowVector3d triangle_area_normal(
   const Eigen::RowVector3d & a, 
   const Eigen::RowVector3d & b, 
   const Eigen::RowVector3d & c)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Replace with your code:
-  ////////////////////////////////////////////////////////////////////////////
-  return Eigen::RowVector3d(0,0,0);
+  RowVector3d l1 = b - a;
+  RowVector3d l2 = c - a;
+  return 0.5 * l1.cross(l2);
 }
