7d030c2d39d06624efb4d43772c90da6099440a6
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-03-08 22:07:33 -0500

    Smoother bump

--- lab6/src/bump_height.glsl
+++ lab6/src/bump_height.glsl
@@ -11,9 +11,9 @@ float bump_height( bool is_moon, vec3 s)
   // Reference: 11.5.3 - Turbulence
   // Reference: http://web.cse.ohio-state.edu/~wang.3602/courses/cse5542-2013-spring/14-noise.pdf
   float w = is_moon ? 2 : 3;
-  float k1 = is_moon ? 4 : 5; // twist factor
-  float k2 = is_moon ? 2 : 4; // density of twists
-  float noise = sqrt( abs (1 + sin( s.x + k1 * improved_perlin_noise(k2 * s) ) / w) );
+  float k1 = is_moon ? 4 : 6; // twist factor
+  float k2 = 2.5; // density of twists
+  float noise = sqrt( abs (1 + cos( s.x + k1 * improved_perlin_noise(k2 * s) ) / w) );
 
-  return smooth_heaviside(noise, is_moon ? 1 : 0.5);
+  return smooth_heaviside(noise, is_moon ? 1.25 : 0.5);
 }
