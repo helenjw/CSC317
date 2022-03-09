bb5e224440bc0aef2eb249062a24cbe7241ede54
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-03-09 09:51:30 -0500

    fix git

--- lab7/include/Skeleton.h
+++
@@ -1,8 +0,0 @@
-#ifndef SKELETON_H
-#define SKELETON_H
-#include "Bone.h"
-// A skeleton is just a list of Bones. Each bone knows its parent as an index
-// into this list.
-#include <vector>
-using Skeleton = std::vector<Bone> ;
-#endif 
--- lab7/scripts/keys.m
+++
@@ -1,3 +0,0 @@
-function k = keys(A)
- k = arrayfun(@(i) struct('t',A(i,1),'xzx',A(i,2:4)),1:size(A,1));
-end
