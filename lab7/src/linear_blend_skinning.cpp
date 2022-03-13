19d8e554f9f85215a2c51bf1905130a01b385af7
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-03-12 23:28:04 -0500

    🍉 Implemented skinning

--- lab7/src/linear_blend_skinning.cpp
+++ lab7/src/linear_blend_skinning.cpp
@@ -1,4 +1,5 @@
 #include "linear_blend_skinning.h"
+using namespace Eigen;
 
 void linear_blend_skinning(
   const Eigen::MatrixXd & V,
@@ -7,8 +8,24 @@ void linear_blend_skinning(
   const Eigen::MatrixXd & W,
   Eigen::MatrixXd & U)
 {
-  /////////////////////////////////////////////////////////////////////////////
-  // Replace with your code
-  U = V;
-  /////////////////////////////////////////////////////////////////////////////
+  U.resize(V.rows(), V.cols());
+
+  // Iterate through every vertex
+  for (int i = 0; i < V.rows(); i++) {
+    Vector4d v_affine = Vector4d( V(i,0), V(i,1), V(i,2), 1);
+    Vector4d pos = Vector4d::Zero();
+
+    // Iterate through every bone
+    for (int j = 0; j < skeleton.size(); j++) {
+      Bone bone = skeleton[j];
+      if (bone.weight_index != -1) {
+        pos += W(i, bone.weight_index) * ( T[j] * v_affine );
+      }
+    }
+
+    U(i,0) = pos[0];
+    U(i,1) = pos[1];
+    U(i,2) = pos[2];
+  }
 }
+
