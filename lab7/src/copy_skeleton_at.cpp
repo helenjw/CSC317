4151b0a332409c44735ef31fa656a725524068e4
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-03-12 23:37:01 -0500

    🍉 Implemented copy skeleton

--- lab7/src/copy_skeleton_at.cpp
+++ lab7/src/copy_skeleton_at.cpp
@@ -3,9 +3,11 @@ Skeleton copy_skeleton_at(
   const Skeleton & skeleton, 
   const Eigen::VectorXd & A)
 {
-  /////////////////////////////////////////////////////////////////////////////
-  // Replace with your code
   Skeleton copy = skeleton;
+  for (int i = 0; i < skeleton.size(); i++) {
+    copy[i].xzx.x() = A[3 * i];
+    copy[i].xzx.y() = A[3 * i + 1];
+    copy[i].xzx.z() = A[3 * i + 2];
+  }
   return copy;
-  /////////////////////////////////////////////////////////////////////////////
 }
