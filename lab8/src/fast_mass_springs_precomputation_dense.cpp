56d0f42bcec070c9274f914fd23680626c8cdb6a
Helen Jiang <jingwen.jiang@mail.utoronto.ca>
2022-03-28 16:22:10 -0400

    🍯 Dense matrix and precomputation

--- lab8/src/fast_mass_springs_precomputation_dense.cpp
+++ lab8/src/fast_mass_springs_precomputation_dense.cpp
@@ -1,6 +1,8 @@
 #include "fast_mass_springs_precomputation_dense.h"
 #include "signed_incidence_matrix_dense.h"
 #include <Eigen/Dense>
+using namespace Eigen;
+#define w 1e10
 
 bool fast_mass_springs_precomputation_dense(
   const Eigen::MatrixXd & V,
@@ -15,10 +17,33 @@ bool fast_mass_springs_precomputation_dense(
   Eigen::MatrixXd & C,
   Eigen::LLT<Eigen::MatrixXd> & prefactorization)
 {
-  /////////////////////////////////////////////////////////////////////////////
-  // Replace with your code
-  Eigen::MatrixXd Q = Eigen::MatrixXd::Identity(V.rows(),V.rows());
-  /////////////////////////////////////////////////////////////////////////////
+  // Populate r - list of edge lengths
+  r.resize(E.rows());
+  for (int i = 0; i < E.rows(); i++) {
+    auto V0 = V.row( E(i, 0) );
+    auto V1 = V.row( E(i, 1) );
+    r(i) = (V0 - V1).norm();
+  }
+
+  // Populate M - mass matrix
+  M = MatrixXd::Zero(V.rows(), V.rows());
+  for (int i = 0; i < V.rows(); i++) {
+    M(i, i) = m[i];
+  }
+
+  // Construct A - signed incidence matrix
+  signed_incidence_matrix_dense(V.rows(), E, A);
+
+  // Populate C - selection matrix for pinned vertices
+  C = MatrixXd::Zero(b.rows(), V.rows());
+
+  // Q = (kAᵀA) + (M / ΔT²) + Qc
+  MatrixXd Q = k * A.transpose() * A + M / (delta_t * delta_t) + w * C.transpose() * C;
+
+  // Pin vertices - Reference: https://lihd1003.github.io/notebook/csc418/mass_spring_system.html
+  MatrixXd Qc = w * C.transpose() * C; // Qc = wCᵀC
+
+  Q += Qc;
   prefactorization.compute(Q);
-  return prefactorization.info() != Eigen::NumericalIssue;
+  return prefactorization.info() != NumericalIssue;
 }
--- lab8/src/signed_incidence_matrix_dense.cpp
+++ lab8/src/signed_incidence_matrix_dense.cpp
@@ -1,12 +1,14 @@
 #include "signed_incidence_matrix_dense.h"
+using namespace Eigen;
 
 void signed_incidence_matrix_dense(
   const int n,
   const Eigen::MatrixXi & E,
   Eigen::MatrixXd & A)
 {
-  //////////////////////////////////////////////////////////////////////////////
-  // Replace with your code
-  A = Eigen::MatrixXd::Zero(E.rows(),n);
-  //////////////////////////////////////////////////////////////////////////////
+  A = MatrixXd::Zero(E.rows(), n);
+  for (int i = 0; i < E.rows(); i++) {
+    A(i, E(i, 0)) = 1;
+    A(i, E(i, 1)) = -1;
+  }
 }
